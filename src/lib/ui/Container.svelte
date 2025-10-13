0a055086ed21579862c3f9e6982d9106797c5950
Jxavier <182994886+JxavierP@users.noreply.github.com>
2025-10-13 15:11:20 -0400

    init: late git initialization

---
+++ src/lib/ui/Container.svelte
@@ -0,0 +1,12 @@
+<div class="container {$$props.class}" {...$$restProps}>
+	<slot />
+</div>
+
+<style>
+	.container {
+		display: flex;
+		max-width: 1650px;
+		margin: 0 auto;
+		padding: 0 1rem;
+	}
+</style>
---
+++ src/lib/ui/Header.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Container from './Container.svelte';
+</script>
+
+<header class="flex h-20 items-center bg-gray-800 text-white/85">
+	<nav class="mx-auto flex w-full max-w-[1650px] justify-between">
+		<div>Slovenia</div>
+		<div>Contact Us</div>
+	</nav>
+</header>
