c642c391d376aaad03e9364dca7c3baa051ded7b
Jxavier Paul <182994886+JxavierP@users.noreply.github.com>
2025-10-15 22:30:06 +0000

    wip: added fonts for header

--- src/lib/ui/Header.svelte
+++ src/lib/ui/Header.svelte
@@ -2,9 +2,9 @@
 	import Container from './Container.svelte';
 </script>
 
-<header class="flex h-20 items-center bg-green-700/60 text-white/85">
-	<nav class="mx-auto flex w-full max-w-[1650px] items-center justify-between">
-		<div class="font-bebas text-3xl">Slovenia Travels</div>
-		<div>Book A Trip</div>
+<header class="flex h-20 items-center bg-green-600/65 text-white">
+	<nav class="mx-auto flex w-[94%] items-center justify-between">
+	<div class="font-cookie text-4xl">Slovenia Travels</div>
+	<div class="font-bebas text-2xl">Book A Trip</div>
 	</nav>
 </header>
