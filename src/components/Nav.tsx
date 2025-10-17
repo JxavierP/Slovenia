f163f507b2fcb91e1bb1fcc039bc6b7312ce3c9e
Jxavier Paul <182994886+JxavierP@users.noreply.github.com>
2025-10-17 22:49:14 +0000

    wip: fixing and working on navbar

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,19 +1,41 @@
 import { useLocation } from "@solidjs/router";
 
 export default function Nav() {
-  const location = useLocation();
-  const active = (path: string) =>
-    path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">Home</a>
-        </li>
-        <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
-          <a href="/about">About</a>
-        </li>
-      </ul>
+    <nav class="bg-green-200 text-emerald-900">
+      <div class="mx-auto max-w-[94%] w-full justify-between flex h-20 items-center">
+        <span>menu</span>
+        <span class="font-cookie text-4xl">Slovenia Travels</span>
+        <ul class="flex gap-5">
+          <li class="font-bebas uppercase text-3xl font-semibold">
+            destinations
+          </li>
+          <li class="font-bebas uppercase text-3xl font-semibold">
+            departures
+          </li>
+          <li class="font-bebas uppercase text-3xl font-semibold">
+            our process
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
+
+// export default function Nav() {
+//   const location = useLocation();
+//   const active = (path: string) =>
+//     path == location.pathname ? "border-sky-600" : "border-transparent hover:border-sky-600";
+//   return (
+//     <nav class="bg-sky-800">
+//       <ul class="container flex items-center p-3 text-gray-200">
+//         <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+//           <a href="/">Home</a>
+//         </li>
+//         <li class={`border-b-2 ${active("/about")} mx-1.5 sm:mx-6`}>
+//           <a href="/about">About</a>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
